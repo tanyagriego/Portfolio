d8db0f58ede3b6951f572682a613853072c33d56
tanyagriego <tanya.griego@gmail.com>
2017-10-22 20:48:05 -0700

    added an array

--- home.js
+++ home.js
@@ -1,6 +1,14 @@
-var Project = function (title, date, description, technologies) {
+var Project = function (title, date, description, technologies, repo) {
   this.title = title;
   this.date = date;
   this.description = description;
   this.technologies = technologies;
 };
+
+var projects = [
+  title: "Crop Swap",
+  img: "img.jpg";
+  desc: "something",
+  repo: "linkhere",
+  tech: "techUsed",
+]

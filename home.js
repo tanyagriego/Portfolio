916f0096a5b1c642c60c22ebb4a6222b62df3e82
tanyagriego <tanya.griego@gmail.com>
2017-10-23 19:57:05 -0700

    added array with projects

--- home.js
+++ home.js
@@ -1,14 +1,51 @@
-var Project = function (title, date, description, technologies, repo) {
+var Project = function (title, date, image, link, description, technologies, repo) {
   this.title = title;
   this.date = date;
+  this.image= image;
+  this.link= link;
   this.description = description;
   this.technologies = technologies;
+  this.repo = repo;
 };
 
 var projects = [
-  title: "Crop Swap",
-  img: "img.jpg";
-  desc: "something",
-  repo: "linkhere",
-  tech: "techUsed",
+  {
+    title: "Crop Swap",
+    date:
+    image: "img.jpg";
+    link:
+    description: "something",
+    technologies: "techUsed",
+    repo: "linkhere",
+  };
+
+  {
+    title: "Grandmother Trivia",
+    date:
+    image: "img.jpg";
+    link:
+    description: "something",
+    technologies: "techUsed",
+    repo: "linkhere",
+  };
+
+  {
+    title: "Market analysis App",
+    date:
+    image: "img.jpg";
+    link:
+    description: "something",
+    technologies: "techUsed",
+    repo: "linkhere",
+  };
+
+  {
+    title: "Salmon Cookies",
+    date:
+    image: "img.jpg";
+    link:
+    description: "something",
+    technologies: "techUsed",
+    repo: "linkhere",
+  };
 ]

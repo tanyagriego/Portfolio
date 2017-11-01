56daabfa807e04d59474e7a812c135f46a7f29eb
tanyagriego <tanya.griego@gmail.com>
2017-11-01 10:28:09 -0700

    Reorganized files

--- app.js
+++
@@ -1,9 +0,0 @@
-function showPreview() {
-  var contentElement = document.getElementById("content");
-  var convertedContent = marked (contentElement.value);
-  document.getElementById("preview".innerHtml) = convertedContent;
-}
-
-window.addEventListener("load", function()) {
-  document.getElementById("showPreview").addEventListener(showPreview);
-});

1c6f4e51be508baca00f951c011b9c5f42e22a8b
tanyagriego <tanya.griego@gmail.com>
2017-11-02 19:19:54 -0700

    Fixed handlebars functionality. Added photos to projects array

--- js/home.js
+++ js/home.js
@@ -1,51 +1,35 @@
+var projectArtciles= [];
+
 function Project (projectInfo) {
-  this.title = projectInfo.title;
+  this.title= projectInfo.title;
   this.image= projectInfo.image;
   this.link= projectInfo.link;
-  this.date = projectInfo.date;
-  this.description = projectInfo.description;
-  this.technologies = projectInfo.technologies;
-  this.repo = projectInfo.repo;
+  this.date= projectInfo.date;
+  this.description= projectInfo.description;
+  this.technologies= projectInfo.technologies;
+  this.repo= projectInfo.repo;
 };
-
-Project.prototype.toHtml = function () {
-  var $newProject = $('.project-template').clone();
-  $newProject.removeClass("project-template");
-  $newProject.find ('.title').text(this.title);
-  $newProject.find('img').attr('src', this.image);
-  $newProject.find('.link').text(this.link);
-  $newProject.find('.date').text(this.date);
-  $newProject.find('.description').text(this.description);
-  $newProject.find('.technologies').text(this.technologies);
-  $newProject.find('.repo').text(this.repo);
-  return $newProject;
-};
-
-projects.forEach(function(project) {
-  var newProject = new Project (project);
-  $('#projects').append(newProject.toHtml());
-});
 
 function handleMainNav() {
   $('.main-nav').on('click', '.tab', function() {
     $('.tab-content').hide();
-    var tabName = '#' + $(this).attr('data-content');
+    var tabName= '#' + $(this).attr('data-content');
     $(tabName).show();
   })
 };
 
-Project.prototype.toHtml = function() {
-var templateFiller = Handlebars.compile( $('#project-templateHB').html() );
-var filledTemplate = templateFiller( this );
+Project.prototype.toHtml= function() {
+var templateFiller= Handlebars.compile( $('#project-templateHB').html() );
+var filledTemplate= templateFiller(this);
 return filledTemplate;
 }
 
-projects.forEach ( project => {
-  projects.push (new Project (proj  ect));
+projects.forEach(project => {
+  projectArtciles.push (new Project (project));
 });
 
-Project.forEach ( project => {
-$( '#projects').append( project.toHtml());
+projectArtciles.forEach(project => {
+$('#projects').append(project.toHtml());
 });
 
 $(document).ready(function() {
